bba0252c7a7a86ba4ca22b8869f0470cb171032b
seokhyun <letushyun@gmail.com>
2023-07-31 03:05:28 +0900

    c

---
+++ CodingTest/CodingTest/BJLecture/Basic1/300/1929_소수구하기_백준풀이.swift
@@ -0,0 +1,30 @@
+//
+//  1929.swift
+//  소수 구하기
+//
+//  Created by SeokHyun on 2023/07/31.
+//
+
+import Foundation
+
+let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+let start = input[0]
+let end = input[1]
+var isChecked = Array(repeating: false, count: 1_000_001)
+var primeArr = [Int]()
+
+for num in 2...end {
+  if isChecked[num] == false { // 지워져 있지 않으면,
+    if start <= num { // start보다 큰 소수의 경우에만
+      primeArr.append(num) // 소수 추가
+    }
+    
+    var j = num*num // i*2 or i+i
+    while j <= end {
+      isChecked[j] = true
+      j += num // 배수만큼 index를 건너뜀
+    }
+  }
+}
+
+print(primeArr.map{String($0)}.joined(separator: "\n"))
--- CodingTest/CodingTest/BJLecture/Basic1/300/main.swift
+++ CodingTest/CodingTest/BJLecture/Basic1/300/main.swift
@@ -1,11 +1,26 @@
 //
-//  main.swift
-//  CodingTest
+//  9020.swift
+//  골드바흐의 추측
 //
-//  Created by SeokHyun on 2023/07/28.
+//  Created by SeokHyun on 2023/07/31.
 //
 
 import Foundation
 
-let a = (-2%3+3)%3
-print(a)
+while let input = readLine() {
+  let end = Int(input)!
+  var isChecked = Array(repeating: false, count: 10_001)
+  var primeArr = [Int]()
+  
+  for num in 2...end {
+    if isChecked[num] == false {
+      primeArr.append(num)
+      
+      var j = num * 2
+      while j <= end {
+        isChecked[j] = true
+        j += num
+      }
+    }
+  }
+}
