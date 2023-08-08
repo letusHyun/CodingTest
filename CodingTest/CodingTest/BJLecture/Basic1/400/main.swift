2314f89a626bad032d1e0af360debe783523729b
seokhyun <letushyun@gmail.com>
2023-08-09 04:03:18 +0900

    c

---
+++ CodingTest/CodingTest/BJLecture/Basic1/400/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  CodingTest
+//
+//  Created by SeokHyun on 2023/08/09.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split{$0==" "}.map{Int(String($0))!}
+var d = Array(repeating: 0, count: 1000)
+for i in 0..<arr.count {
+  d[i] = 1
+  for j in 
+}
