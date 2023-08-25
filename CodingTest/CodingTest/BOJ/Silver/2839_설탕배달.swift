87655c96593db8cd386db245c13d23714d736896
seokhyun <letushyun@gmail.com>
2023-08-25 17:11:05 +0900

    c

---
+++ CodingTest/CodingTest/BOJ/Silver/2839_설탕배달.swift
@@ -0,0 +1,31 @@
+//
+//  2839.swift
+//  설탕배달
+//
+//  Created by SeokHyun on 2023/08/25.
+//
+
+let n = Int(readLine()!)!
+// d[n][k]의 의미: 마지막수가 5일때의 최소 개수와 마지막수가 3일때의 최소 개수중에서 최솟값 + 1
+// 3*1667 = 5001이므로 봉지의 개수 out of index를 1667로 설정
+var d = Array(repeating: 1667, count: 5001)
+
+d[3] = 1
+d[5] = 1
+
+if 1<=n, n<=5 {
+  if d[n] >= 1667 {
+    print(-1)
+  } else {
+    print(d[n])
+  }
+} else {
+  for i in 6...n {
+    d[i] = min(d[i-3], d[i-5]) + 1
+  }
+  if d[n] >= 1667 {
+    print(-1)
+  } else {
+    print(d[n])
+  }
+}
